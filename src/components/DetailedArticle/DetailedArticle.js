aa8d5867e9f046f0c0e1e19454b24c8295903336
Steven Mancine <stevenrmancine@gmail.com>
2021-06-02 16:32:16 -0600

    Add link to full article

--- src/components/DetailedArticle/DetailedArticle.js
+++ src/components/DetailedArticle/DetailedArticle.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Article from '../Article/Article';
+import { Link } from 'react-router-dom';
 
 const DetailedArticle = ({ story }) => {
   return (
     !story ? <h1>Loading.....</h1> : 
-      <Article
-          title={story.title}
-          img={story.image?.url}
-          alt={story.image?.caption}
-          byline={story.byline}
-          publishedDate={story.publishedDate}
-          uri={story.uri}
-      />
+      <article className="detailed-article">
+        <img src={story.image?.url} alt={story.image?.caption}/>
+        <h2>{story.title}</h2>
+        <p>{story.abstract}</p>
+        <p>{story.byline}</p>
+        <p>{story.publishedDate}</p>
+        <a href={story.url}><button>Full Story</button></a>
+      </article>
   )
 }
 
