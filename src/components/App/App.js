69d5e170763cbe8347daaac23a5282b45fdee265
Steven Mancine <stevenrmancine@gmail.com>
2021-06-02 16:23:49 -0600

    Add home button to all pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { getTopStories } from '../../utilities/APICalls';
 import { cleanData } from '../../utilities/cleanData';
 import Stories from '../Stories/Stories';
-import { Switch, Route } from 'react-router-dom';
-import DetailedArticle from '../DetailedArticle/DetailedArticle'
+import { Switch, Route, Link } from 'react-router-dom';
+import DetailedArticle from '../DetailedArticle/DetailedArticle';
+import { IoIosHome } from 'react-icons/io';
 
 const App = () => {
   const [topStories, setTopStories] = useState([]);
@@ -14,12 +15,16 @@ const App = () => {
 
   return (
     <div className="App">
-     <Switch>
+      <header className="header">
+        <Route path="/" render={({match}) => {
+          return !match.isExact && <Link to="/"><IoIosHome className="home-button"/></Link> 
+        }} />
+        <h1>News of Today</h1>
+      </header>
+      <Switch>
         <Route exact path="/" render={() => <Stories topStories={topStories}/> } />
         <Route exact path="/article/:id" render={({ match }) => {
-          const { id } = match.params;
-          const story = topStories.find(story => story.uri.includes(id));
-          console.log(story);
+          const story = topStories.find(story => story.uri.includes(match.params.id));
           return <DetailedArticle story={story} />
         }} />
       </Switch>
--- src/components/DetailedArticle/DetailedArticle.js
+++ src/components/DetailedArticle/DetailedArticle.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Article from '../Article/Article'
+import Article from '../Article/Article';
 
 const DetailedArticle = ({ story }) => {
-  // const article = topStories.find(story => story.uri === id)
   return (
-    <Article
-        title={story.title}
-        img={story.image?.url}
-        alt={story.image?.caption}
-        byline={story.byline}
-        publishedDate={story.publishedDate}
-        uri={story.uri}
+    !story ? <h1>Loading.....</h1> : 
+      <Article
+          title={story.title}
+          img={story.image?.url}
+          alt={story.image?.caption}
+          byline={story.byline}
+          publishedDate={story.publishedDate}
+          uri={story.uri}
       />
   )
 }
