b393f9817b20c29864c0ed867e14fdc563817933
Steven Mancine <stevenrmancine@gmail.com>
2021-06-03 09:43:20 -0600

    Fix React setState issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getTopStories } from '../../utilities/APICalls';
+import { getArticles } from '../../utilities/APICalls';
 import { cleanData } from '../../utilities/cleanData';
 import Stories from '../Stories/Stories';
 import { Switch, Route, Link } from 'react-router-dom';
@@ -9,28 +9,45 @@ import Nav from '../Nav/Nav'
 import { HiMenuAlt3 } from 'react-icons/hi'
 
 const App = () => {
-  const [topStories, setTopStories] = useState([]);
-  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const [topArticles, setTopArticles] = useState([]);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [section, setSection] = useState('');
+  const [sortedArticles, setSortedArticles] = useState([]);
 
   useEffect(() => {
-    getTopStories().then(data => setTopStories(cleanData(data)))
-  }, [])
+    getArticles().then(data => setTopArticles(cleanData(data)));
+  }, []);
+  
+  useEffect(() => {
+    // const path = window.location.path  Use this option if the setTimeout in route is a code smell
+    // if path.includes('/section') setSection with the key that follows
+    section && getArticles(section).then(data => setSortedArticles(cleanData(data)));
+  }, [section])
 
   return (
     <div className="App">
       <header className="header">
         <Route path="/" render={({match}) => {
-          return !match.isExact && <Link to="/"><IoIosHome className="home-button" tabIndex='0'/></Link> 
+          return (
+            !match.isExact && 
+            <Link to="/">
+              <IoIosHome className="home-button" tabIndex='0'/>
+            </Link> 
+          )
         }} />
         <h1>News of Today</h1>
         <HiMenuAlt3 className='hamburger' onClick={() => setMenuIsOpen(!menuIsOpen)} tabIndex='0'/>
       </header>
-      {menuIsOpen && <Nav toggle={() => setMenuIsOpen(!menuIsOpen)}/>}
+      {menuIsOpen && <Nav toggle={() => setMenuIsOpen(!menuIsOpen)} />}
       <Switch>
-        <Route exact path="/" render={() => <Stories topStories={topStories}/> } />
+        <Route exact path="/" render={() => <Stories articles={topArticles} /> } />
         <Route exact path="/article/:id" render={({ match }) => {
-          const story = topStories.find(story => story.uri.includes(match.params.id));
-          return <DetailedArticle story={story} />
+          const story = topArticles.find(story => story.uri.includes(match.params.id));
+          return <DetailedArticle story={story} />;
+        }} />
+        <Route exact path="/section/:section" render={({ match }) => {
+          setTimeout(() => setSection(match.params.section), 0)
+          return !sortedArticles.length ? <h1>loading...</h1> : <Stories articles={sortedArticles}/>
         }} />
       </Switch>
     </div>
